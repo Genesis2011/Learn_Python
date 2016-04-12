c78f47c5f0bbcc9df5dfd28c3e6aa7ca7009203f
Genesis2011 <2576340060@qq.com>
2016-04-12 13:28:15 +0800

    ex3.py
    添加了浮点数的显示

--- lihoufeng/ex3.py
+++ lihoufeng/ex3.py
@@ -1,11 +1,21 @@
 print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
+# float
+print "float:"
+print "Hens", "%f" %(25 + 30 / 6)
+
 print "Roosters", 100 - 25 * 3 % 4
+# float
+print "float:"
+print "Roosters", "%f" %(100 - 25 * 3 % 4)
 
 print "Now I will count the eggs:"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# float
+print "float:"
+print "%f" %(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
