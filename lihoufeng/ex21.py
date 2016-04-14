02c36e82aee054f264d45d870ce11b8855bb3204
Genesis2011 <2576340060@qq.com>
2016-04-14 17:34:08 +0800

    add
    21~25

---
+++ lihoufeng/ex21.py
@@ -0,0 +1,40 @@
+def add(a, b):
+    print "ADDING %d + %d" % (a,b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
+
+# By hand
+def puzzle(age, height, weight, iq):
+    result = iq / 2
+    result *= weight
+    result = height - result
+    result += age
+    return result
+
+print "By hand: ", puzzle(age, height, weight, iq)
